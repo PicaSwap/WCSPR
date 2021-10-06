2c0fcf073efb0e86bca1fda3c9f0bebbbfc2ded2
Yura Kriachko <iurii.kriachko@gmail.com>
2021-10-06 02:41:39 +0200

    move helpers to a separate file

---
+++ contracts/src/helpers.rs
@@ -0,0 +1,79 @@
+use casper_erc20::Error;
+use casper_erc20::Address;
+use casper_types::bytesrepr::ToBytes;
+use casper_types::CLTyped;
+use casper_types::bytesrepr::FromBytes;
+use casper_contract::{contract_api::{runtime, storage}, unwrap_or_revert::UnwrapOrRevert};
+use casper_types::{URef, system::CallStackElement};
+use core::convert::TryInto;
+
+// Helper functions
+
+pub fn set_main_purse(purse: URef) {
+    runtime::put_key("main_purse", purse.into())
+}
+
+pub fn get_main_purse() -> URef {
+    let contract_main_purse_key = runtime::get_key("main_purse").unwrap_or_revert();
+    let contract_main_purse = contract_main_purse_key.as_uref().unwrap_or_revert();
+    *contract_main_purse 
+}
+
+pub fn get_key<T: FromBytes + CLTyped>(name: &str) -> Option<T> {
+    match runtime::get_key(name) {
+        None => None,
+        Some(value) => {
+            let key = value.try_into().unwrap_or_revert();
+            let result = storage::read(key).unwrap_or_revert().unwrap_or_revert();
+            Some(result)
+        }
+    }
+}
+
+pub fn set_key<T: ToBytes + CLTyped>(name: &str, value: T) {
+    match runtime::get_key(name) {
+        Some(key) => {
+            let key_ref = key.try_into().unwrap_or_revert();
+            storage::write(key_ref, value);
+        }
+        None => {
+            let key = storage::new_uref(value).into();
+            runtime::put_key(name, key);
+        }
+    }
+}
+
+/// Gets the immediate call stack element of the current execution.
+pub fn get_immediate_call_stack_item() -> Option<CallStackElement> {
+    let call_stack = runtime::get_call_stack();
+    call_stack.into_iter().rev().nth(1)
+}
+
+/// Returns address based on a [`CallStackElement`].
+///
+/// For `Session` and `StoredSession` variants it will return account hash, and for `StoredContract`
+/// case it will use contract hash as the address.
+pub fn call_stack_element_to_address(call_stack_element: CallStackElement) -> Address {
+    match call_stack_element {
+        CallStackElement::Session { account_hash } => Address::from(account_hash),
+        CallStackElement::StoredSession { account_hash, .. } => {
+            // Stored session code acts in account's context, so if stored session wants to interact
+            // with an ERC20 token caller's address will be used.
+            Address::from(account_hash)
+        }
+        CallStackElement::StoredContract {
+            contract_package_hash,
+            ..
+        } => Address::from(contract_package_hash),
+    }
+}
+
+/// Gets the immediate session caller of the current execution.
+///
+/// This function ensures that only session code can execute this function, and disallows stored
+/// session/stored contracts.
+pub(crate) fn get_immediate_caller_address() -> Result<Address, Error> {
+    get_immediate_call_stack_item()
+        .map(call_stack_element_to_address)
+        .ok_or(Error::InvalidContext)
+}
--- contracts/src/wcspr_contract.rs
+++ contracts/src/wcspr_contract.rs
@@ -7,24 +7,30 @@ compile_error!("target arch should be wasm32: compile with '--target wasm32-unkn
 extern crate alloc;
 
 mod entry_points;
+mod helpers;
+
+use crate::helpers::set_key;
+use crate::helpers::set_main_purse;
+use crate::helpers::get_key;
+use crate::helpers::get_immediate_caller_address;
+use crate::helpers::get_main_purse;
 
 use casper_types::RuntimeArgs;
 use casper_types::ContractHash;
 use casper_types::HashAddr;
 use casper_types::Key;
 use alloc::string::String;
-use core::convert::TryInto;
 
-use casper_contract::{contract_api::{runtime, storage, system}, unwrap_or_revert::UnwrapOrRevert};
+use casper_contract::{contract_api::{runtime, system}, unwrap_or_revert::UnwrapOrRevert};
 use casper_erc20::{
     constants::{
         ADDRESS_RUNTIME_ARG_NAME, AMOUNT_RUNTIME_ARG_NAME, DECIMALS_RUNTIME_ARG_NAME,
         NAME_RUNTIME_ARG_NAME, OWNER_RUNTIME_ARG_NAME, RECIPIENT_RUNTIME_ARG_NAME,
         SPENDER_RUNTIME_ARG_NAME, SYMBOL_RUNTIME_ARG_NAME, TOTAL_SUPPLY_RUNTIME_ARG_NAME,
     },
-    Address, ERC20, Error
+    Address, ERC20
 };
-use casper_types::{CLValue, CLTyped, U256, URef, U512, bytesrepr::{FromBytes, ToBytes}, system::CallStackElement};
+use casper_types::{CLValue, U256, URef, U512};
 
 // TODO: understand why changing the name makes everything crash
 //const CONTRACT_KEY_NAME: &str = "wcspr_token_contract";
@@ -107,17 +113,14 @@ pub extern "C" fn deposit() {
     let cspr_amount_u256: U256 = U256::from(cspr_amount.as_u128());
 
     // TODO: why is it not working???
-    //let contract_main_purse: URef = get_key("main_purse").unwrap_or_revert();
+    let contract_main_purse : URef = get_main_purse();
 
-    let contract_main_purse_key = runtime::get_key("main_purse").unwrap_or_revert();
-    let contract_main_purse = contract_main_purse_key.as_uref().unwrap_or_revert();
-
-    let main_purse_balance : U512 = system::get_purse_balance(*contract_main_purse).unwrap_or_revert();
+    let main_purse_balance : U512 = system::get_purse_balance(contract_main_purse).unwrap_or_revert();
     // TODO: this line causes test crash with ForgedReference URef
     // Save CSPR provided by user into our contract
-    let _ = system::transfer_from_purse_to_purse(tmp_purse, *contract_main_purse, cspr_amount, None);
+    let _ = system::transfer_from_purse_to_purse(tmp_purse, contract_main_purse, cspr_amount, None);
 
-    let main_purse_balance_after : U512 = system::get_purse_balance(*contract_main_purse).unwrap_or_revert();
+    let main_purse_balance_after : U512 = system::get_purse_balance(contract_main_purse).unwrap_or_revert();
     assert_eq!(main_purse_balance + cspr_amount, main_purse_balance_after);
 
     // Get account of the user who called the contract
@@ -139,13 +142,12 @@ pub extern "C" fn withdraw() {
 
     let balance = ERC20::default().balance_of(sender);
 
-    let contract_main_purse_key = runtime::get_key("main_purse").unwrap_or_revert();
-    let contract_main_purse = contract_main_purse_key.as_uref().unwrap_or_revert();
-    let main_purse_balance : U512 = system::get_purse_balance(*contract_main_purse).unwrap_or_revert();
+    let contract_main_purse = get_main_purse();
+    let main_purse_balance : U512 = system::get_purse_balance(contract_main_purse).unwrap_or_revert();
 
     if balance >= cspr_amount_u256 &&  cspr_amount < main_purse_balance {
         system::transfer_from_purse_to_account(
-            *contract_main_purse, 
+            contract_main_purse, 
             *sender.as_account_hash().unwrap_or_revert(), 
             cspr_amount, 
             None
@@ -160,7 +162,7 @@ pub extern "C" fn init() {
     match value {
         Some(_) => {},
         None => {
-            set_key("main_purse", system::create_purse());
+            set_main_purse(system::create_purse());
             set_key("initialized", true);
         },
     }
@@ -188,67 +190,6 @@ fn call() {
 
     let _: () = runtime::call_contract(contract_hash, "init", RuntimeArgs::new());
 
-}
-
-
-// Helper functions
-
-
-fn get_key<T: FromBytes + CLTyped>(name: &str) -> Option<T> {
-    match runtime::get_key(name) {
-        None => None,
-        Some(value) => {
-            let key = value.try_into().unwrap_or_revert();
-            let result = storage::read(key).unwrap_or_revert().unwrap_or_revert();
-            Some(result)
-        }
-    }
-}
-
-fn set_key<T: ToBytes + CLTyped>(name: &str, value: T) {
-    match runtime::get_key(name) {
-        Some(key) => {
-            let key_ref = key.try_into().unwrap_or_revert();
-            storage::write(key_ref, value);
-        }
-        None => {
-            let key = storage::new_uref(value).into();
-            runtime::put_key(name, key);
-        }
-    }
-}
-
-/// Gets the immediate call stack element of the current execution.
-fn get_immediate_call_stack_item() -> Option<CallStackElement> {
-    let call_stack = runtime::get_call_stack();
-    call_stack.into_iter().rev().nth(1)
-}
-
-/// Returns address based on a [`CallStackElement`].
-///
-/// For `Session` and `StoredSession` variants it will return account hash, and for `StoredContract`
-/// case it will use contract hash as the address.
-fn call_stack_element_to_address(call_stack_element: CallStackElement) -> Address {
-    match call_stack_element {
-        CallStackElement::Session { account_hash } => Address::from(account_hash),
-        CallStackElement::StoredSession { account_hash, .. } => {
-            // Stored session code acts in account's context, so if stored session wants to interact
-            // with an ERC20 token caller's address will be used.
-            Address::from(account_hash)
-        }
-        CallStackElement::StoredContract {
-            contract_package_hash,
-            ..
-        } => Address::from(contract_package_hash),
-    }
-}
-
-/// Gets the immediate session caller of the current execution.
-///
-/// This function ensures that only session code can execute this function, and disallows stored
-/// session/stored contracts.
-pub(crate) fn get_immediate_caller_address() -> Result<Address, Error> {
-    get_immediate_call_stack_item()
-        .map(call_stack_element_to_address)
-        .ok_or(Error::InvalidContext)
+    let main_purse_balance : U512 = system::get_purse_balance(get_main_purse()).unwrap_or_revert();
+    assert_eq!(main_purse_balance, U512::from(0));
 }
