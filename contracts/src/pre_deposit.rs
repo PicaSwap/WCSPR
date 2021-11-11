c7c6b17248389eb60e99422fb324f121896f2512
Loizage <zagemmm@icloud.com>
2021-11-11 14:57:01 +0000

    Limit per protocol for predeposit with test

--- contracts/src/pre_deposit.rs
+++ contracts/src/pre_deposit.rs
@@ -5,13 +5,15 @@ use casper_contract::{
     unwrap_or_revert::UnwrapOrRevert,
 };
 use casper_erc20::Address;
+/*
 use casper_erc20::Error;
 use casper_types::system::CallStackElement;
+*/
 use casper_types::RuntimeArgs;
 use casper_types::{runtime_args, ApiError, ContractHash, HashAddr, Key, URef, U256, U512};
 pub enum DepositError {
     ExceedUserLimit = 0,
-    CannotWithdrawZero = 1,
+    ExceedContractLimit = 1,
 }
 
 impl From<DepositError> for ApiError {
@@ -20,6 +22,74 @@ impl From<DepositError> for ApiError {
     }
 }
 
+#[no_mangle]
+fn call() {
+    // This function is used to accept CSPR from the user and pass to the
+    // main contract that going to swap it to WCSPR
+    // We need it because casper don't allow us to access `get_main_purse` from main contract
+
+    // how many cspr tokens to transfer
+    let cspr_amount: U512 = runtime::get_named_arg("cspr_amount");
+    let cspr_amount_u256: U256 = U256::from(cspr_amount.as_u128());
+
+    // WCSPR contract hash address passed as an argument to this contract
+    let wcspr_contract_key: Key = runtime::get_named_arg("wcspr_contract_hash_key");
+    let _wcspr_contract_hash: HashAddr = wcspr_contract_key.into_hash().unwrap_or_revert();
+    let wcspr_contract_hash: ContractHash = ContractHash::new(_wcspr_contract_hash);
+
+    // Get total WCSPR supply
+    let total_supply: U256 =
+        runtime::call_contract::<U256>(wcspr_contract_hash, "total_supply", runtime_args! {});
+
+    // Stop deposit if total supply exceed contract limits
+    // 200 CSPR (200 * 10**9)
+    // let multiplier: U256 = U256:from(10.pow(9));
+    let multiplier: U256 = (U256::from(10)).pow(U256::from(9));
+    let contract_limit: U256 = U256::from(200) * multiplier;
+
+    if (total_supply + cspr_amount_u256) > contract_limit {
+        runtime::revert(DepositError::ExceedContractLimit)
+    };
+
+    // Get Address (AccountHash) of the user who called the contract
+    let sender: Address = Address::from(runtime::get_caller());
+
+    // Get user's WCSPR balance
+    let sender_wcspr_balance: U256 = runtime::call_contract::<U256>(
+        wcspr_contract_hash,
+        "balance_of",
+        runtime_args! {
+            "address" => sender
+        },
+    );
+
+    // Stop deposit if user exceeds personal limits
+    // 100 CSPR (100 * 10**9)
+    let user_limit: U256 = U256::from(100) * multiplier;
+
+    if (sender_wcspr_balance + cspr_amount_u256) > user_limit {
+        runtime::revert(DepositError::ExceedUserLimit)
+    };
+
+    // Purse with CSPR tokens of the user who call the contract
+    let sender_purse: URef = account::get_main_purse();
+
+    // Here we put tokens we want to transfer to the contract
+    let tmp_purse: URef = system::create_purse();
+
+    // move from sender to tmp purse, so we can use tmp purse in the next contract
+    let _ = system::transfer_from_purse_to_purse(sender_purse, tmp_purse, cspr_amount, None);
+
+    runtime::call_contract(
+        wcspr_contract_hash,
+        "deposit",
+        runtime_args! {
+            "tmp_purse" => tmp_purse
+        },
+    )
+}
+
+/*
 /// Gets the first call stack element of the current execution.
 pub fn get_first_call_stack_item() -> Option<CallStackElement> {
     let call_stack = runtime::get_call_stack();
@@ -53,58 +123,6 @@ pub(crate) fn get_first_caller_address_() -> Result<Address, Error> {
 
 #[no_mangle]
 fn call() {
-    // This function is used to accept CSPR from the user and pass to the
-    // main contract that going to swap it to WCSPR
-    // We need it because casper don't allow us to access `get_main_purse` from main contract
-
-    // how many cspr tokens to transfer
-    let cspr_amount: U512 = runtime::get_named_arg("cspr_amount");
-    let cspr_amount_u256: U256 = U256::from(cspr_amount.as_u128());
-
-    // WCSPR contract hash address passed as an argument to this contract
-    let wcspr_contract_key: Key = runtime::get_named_arg("wcspr_contract_hash_key");
-    let _wcspr_contract_hash: HashAddr = wcspr_contract_key.into_hash().unwrap_or_revert();
-    let wcspr_contract_hash: ContractHash = ContractHash::new(_wcspr_contract_hash);
-
-    // Stop deposit if total supply exceed limits
-    let total_supply: U256 =
-        runtime::call_contract::<U256>(wcspr_contract_hash, "total_supply", runtime_args! {});
-
-    // Get account of the user who called the contract
     let sender: Address = get_first_caller_address_().unwrap_or_revert();
-
-    // Check if user does not exceed limits
-    // Read how much user has WCSPR on it's balance
-    let sender_wcspr_balance: U256 = runtime::call_contract::<U256>(
-        wcspr_contract_hash,
-        "balance_of",
-        runtime_args! {
-            "address" => sender
-        },
-    );
-
-    let multiplier: U256 = (U256::from(10)).pow(U256::from(9));
-    let limit: U256 = U256::from(100);
-    let user_limit: U256 = limit * multiplier;
-
-    if (sender_wcspr_balance + cspr_amount_u256) >= user_limit {
-        runtime::revert(DepositError::ExceedUserLimit)
-    };
-
-    // Purse with CSPR tokens of the user who call the contract
-    let sender_purse: URef = account::get_main_purse();
-
-    // Here we put tokens we want to transfer to the contract
-    let tmp_purse: URef = system::create_purse();
-
-    // move from sender to tmp purse, so we can use tmp purse in the next contract
-    let _ = system::transfer_from_purse_to_purse(sender_purse, tmp_purse, cspr_amount, None);
-
-    runtime::call_contract(
-        wcspr_contract_hash,
-        "deposit",
-        runtime_args! {
-            "tmp_purse" => tmp_purse
-        },
-    )
 }
+*/
--- tests/src/integration_tests.rs
+++ tests/src/integration_tests.rs
@@ -3,7 +3,7 @@ mod test_fixture;
 
 #[cfg(test)]
 mod tests {
-    use casper_engine_test_support::TestContext;
+    // use casper_engine_test_support::TestContext;
     use casper_types::{Key, U256, U512};
 
     use crate::test_fixture::{Sender, TestFixture};
@@ -16,7 +16,7 @@ mod tests {
         assert_eq!(fixture.token_decimals(), TestFixture::TOKEN_DECIMALS);
         assert_eq!(
             fixture.balance_of(Key::from(fixture.ali)),
-            Some(TestFixture::token_total_supply())
+            Some(fixture.token_total_supply())
         );
     }
 
@@ -40,24 +40,39 @@ mod tests {
         assert_eq!(fixture.cspr_balance(), cspr_deposit_amount);
     }
 
-    #[should_panic(expected = "ApiError::User(0) [65536]")]
     #[test]
-    fn should_not_deposit_more_then_limit_1() {
+    fn should_deposit_max_user_limit() {
         let mut fixture = TestFixture::install_contract();
-
-        let cspr_deposit_amount = U512::from(101) * (U512::from(10)).pow(U512::from(9));
-        let deposited_wcspr = U256::from(101) * (U256::from(10)).pow(U256::from(9));
+        let cspr_deposit_amount = U512::from(100) * (U512::from(10)).pow(U512::from(9));
+        let deposited_wcspr = U256::from(100) * (U256::from(10)).pow(U256::from(9));
         let sender = Sender(fixture.ali);
 
         let initial_balance = fixture.balance_of(Key::from(fixture.ali)).unwrap();
         let expected_balance = initial_balance + deposited_wcspr;
 
         fixture.deposit(sender, cspr_deposit_amount);
+
+        assert_eq!(
+            fixture.balance_of(Key::from(fixture.ali)),
+            Some(expected_balance)
+        );
+        assert_eq!(fixture.cspr_balance(), cspr_deposit_amount);
     }
 
     #[should_panic(expected = "ApiError::User(0) [65536]")]
     #[test]
-    fn should_not_deposit_more_then_limit_2() {
+    fn should_not_deposit_more_then_user_limit_1() {
+        let mut fixture = TestFixture::install_contract();
+
+        let cspr_deposit_amount = U512::from(101) * (U512::from(10)).pow(U512::from(9));
+        let sender = Sender(fixture.ali);
+
+        fixture.deposit(sender, cspr_deposit_amount);
+    }
+
+    #[should_panic(expected = "ApiError::User(0) [65536]")]
+    #[test]
+    fn should_not_deposit_more_then_user_limit_2() {
         let mut fixture = TestFixture::install_contract();
 
         let cspr_deposit_amount = U512::from(55) * (U512::from(10)).pow(U512::from(9));
@@ -76,6 +91,62 @@ mod tests {
         assert_eq!(fixture.cspr_balance(), cspr_deposit_amount);
 
         fixture.deposit(sender, cspr_deposit_amount);
+    }
+
+    #[should_panic(expected = "ApiError::User(1) [65537]")]
+    #[test]
+    fn should_not_deposit_more_then_contract_limit() {
+        let mut fixture = TestFixture::install_contract();
+
+        let cspr_deposit_amount = U512::from(100) * (U512::from(10)).pow(U512::from(9));
+        let deposited_wcspr = U256::from(100) * (U256::from(10)).pow(U256::from(9));
+        let transfered_wcspr = deposited_wcspr;
+        let ali = Sender(fixture.ali);
+        let bob = Sender(fixture.bob);
+
+        let initial_balance_ali = fixture.balance_of(Key::from(fixture.ali)).unwrap();
+        let expected_balance_ali = initial_balance_ali + deposited_wcspr;
+
+        // ali deposits MAX CSPR amount
+        fixture.deposit(ali, cspr_deposit_amount);
+
+        assert_eq!(
+            fixture.balance_of(Key::from(fixture.ali)),
+            Some(expected_balance_ali)
+        );
+        assert_eq!(fixture.cspr_balance(), cspr_deposit_amount);
+
+        // bob deposits MAX CSPR amount
+        let initial_balance_bob = U256::from(0);
+        let expected_balance_bob = initial_balance_bob + deposited_wcspr;
+
+        fixture.deposit(bob, cspr_deposit_amount);
+
+        assert_eq!(
+            fixture.balance_of(Key::from(fixture.bob)),
+            Some(expected_balance_bob)
+        );
+        assert_eq!(fixture.cspr_balance(), cspr_deposit_amount * 2);
+
+        // ali transfers to joe all the WCSPR
+        assert_eq!(fixture.balance_of(Key::from(fixture.joe)), None);
+
+        fixture.transfer(
+            Key::from(fixture.joe),
+            transfered_wcspr,
+            Sender(fixture.ali),
+        );
+        assert_eq!(
+            fixture.balance_of(Key::from(fixture.joe)),
+            Some(transfered_wcspr)
+        );
+        assert_eq!(
+            fixture.balance_of(Key::from(fixture.ali)),
+            Some(expected_balance_ali - transfered_wcspr)
+        );
+
+        // ali deposits MAX CSPR amount again
+        fixture.deposit(ali, cspr_deposit_amount);
     }
 
     #[test]
@@ -113,10 +184,28 @@ mod tests {
     #[test]
     fn should_transfer() {
         let mut fixture = TestFixture::install_contract();
+
+        // Deposit WCPSR to ali balance
+        let cspr_deposit_amount = U512::from(1000);
+        let deposited_wcspr = U256::from(1000);
+        let sender = Sender(fixture.ali);
+
+        let initial_balance = fixture.balance_of(Key::from(fixture.ali)).unwrap();
+        let expected_balance = initial_balance + deposited_wcspr;
+
+        fixture.deposit(sender, cspr_deposit_amount);
+
+        assert_eq!(
+            fixture.balance_of(Key::from(fixture.ali)),
+            Some(expected_balance)
+        );
+        assert_eq!(fixture.cspr_balance(), cspr_deposit_amount);
+
+        // Make transfer from ali to bob
         assert_eq!(fixture.balance_of(Key::from(fixture.bob)), None);
         assert_eq!(
             fixture.balance_of(Key::from(fixture.ali)),
-            Some(TestFixture::token_total_supply())
+            Some(fixture.token_total_supply())
         );
         let transfer_amount_1 = U256::from(42);
         fixture.transfer(
@@ -130,9 +219,10 @@ mod tests {
         );
         assert_eq!(
             fixture.balance_of(Key::from(fixture.ali)),
-            Some(TestFixture::token_total_supply() - transfer_amount_1)
+            Some(fixture.token_total_supply() - transfer_amount_1)
         );
 
+        // Make transfer from bob to ali
         let transfer_amount_2 = U256::from(20);
         fixture.transfer(
             Key::from(fixture.ali),
@@ -141,7 +231,7 @@ mod tests {
         );
         assert_eq!(
             fixture.balance_of(Key::from(fixture.ali)),
-            Some(TestFixture::token_total_supply() - transfer_amount_1 + transfer_amount_2),
+            Some(fixture.token_total_supply() - transfer_amount_1 + transfer_amount_2),
         );
         assert_eq!(
             fixture.balance_of(Key::from(fixture.bob)),
@@ -150,9 +240,89 @@ mod tests {
     }
 
     #[test]
+    fn should_transfer_from() {
+        let approve_amount = U256::from(100);
+        let transfer_amount = U256::from(42);
+        assert!(approve_amount > transfer_amount);
+
+        let mut fixture = TestFixture::install_contract();
+
+        let owner = fixture.ali;
+        let spender = fixture.bob;
+        let recipient = fixture.joe;
+
+        // Deposit WCPSR to ali balance
+        let cspr_deposit_amount = U512::from(1000);
+        let deposited_wcspr = U256::from(1000);
+        let sender = Sender(fixture.ali);
+
+        let initial_balance = fixture.balance_of(Key::from(fixture.ali)).unwrap();
+        let expected_balance = initial_balance + deposited_wcspr;
+
+        fixture.deposit(sender, cspr_deposit_amount);
+
+        assert_eq!(
+            fixture.balance_of(Key::from(fixture.ali)),
+            Some(expected_balance)
+        );
+        assert_eq!(fixture.cspr_balance(), cspr_deposit_amount);
+
+        // Approve ali WCSPR to be spent by bob
+        let owner_balance_before = fixture
+            .balance_of(Key::from(owner))
+            .expect("owner should have balance");
+        fixture.approve(Key::from(spender), approve_amount, Sender(owner));
+        assert_eq!(
+            fixture.allowance(Key::from(owner), Key::from(spender)),
+            Some(approve_amount)
+        );
+
+        // Bob transfer WCSPR of ali to joe
+        fixture.transfer_from(
+            Key::from(owner),
+            Key::from(recipient),
+            transfer_amount,
+            Sender(spender),
+        );
+
+        assert_eq!(
+            fixture.balance_of(Key::from(owner)),
+            Some(owner_balance_before - transfer_amount),
+            "should decrease balance of the owner"
+        );
+        assert_eq!(
+            fixture.allowance(Key::from(owner), Key::from(spender)),
+            Some(approve_amount - transfer_amount),
+            "should decrease allowance of the spender"
+        );
+        assert_eq!(
+            fixture.balance_of(Key::from(recipient)),
+            Some(transfer_amount),
+            "recipient should receive tokens"
+        );
+    }
+
+    #[test]
     fn should_transfer_full_amount() {
         let mut fixture = TestFixture::install_contract();
 
+        // Deposit WCPSR to ali balance
+        let cspr_deposit_amount = U512::from(1000);
+        let deposited_wcspr = U256::from(1000);
+        let sender = Sender(fixture.ali);
+
+        let initial_balance = fixture.balance_of(Key::from(fixture.ali)).unwrap();
+        let expected_balance = initial_balance + deposited_wcspr;
+
+        fixture.deposit(sender, cspr_deposit_amount);
+
+        assert_eq!(
+            fixture.balance_of(Key::from(fixture.ali)),
+            Some(expected_balance)
+        );
+        assert_eq!(fixture.cspr_balance(), cspr_deposit_amount);
+
+        // Transfer all WCSPR from ali to bob
         let initial_ali_balance = fixture.balance_of(Key::from(fixture.ali)).unwrap();
         assert_eq!(fixture.balance_of(Key::from(fixture.bob)), None);
 
@@ -171,6 +341,7 @@ mod tests {
             Some(U256::zero())
         );
 
+        // Transfer all WCSPR from bob to ali
         fixture.transfer(
             Key::from(fixture.ali),
             initial_ali_balance,
@@ -199,51 +370,6 @@ mod tests {
             Key::from(fixture.bob),
             initial_ali_balance + U256::one(),
             Sender(fixture.ali),
-        );
-    }
-
-    #[test]
-    fn should_transfer_from() {
-        let approve_amount = U256::from(100);
-        let transfer_amount = U256::from(42);
-        assert!(approve_amount > transfer_amount);
-
-        let mut fixture = TestFixture::install_contract();
-
-        let owner = fixture.ali;
-        let spender = fixture.bob;
-        let recipient = fixture.joe;
-
-        let owner_balance_before = fixture
-            .balance_of(Key::from(owner))
-            .expect("owner should have balance");
-        fixture.approve(Key::from(spender), approve_amount, Sender(owner));
-        assert_eq!(
-            fixture.allowance(Key::from(owner), Key::from(spender)),
-            Some(approve_amount)
-        );
-
-        fixture.transfer_from(
-            Key::from(owner),
-            Key::from(recipient),
-            transfer_amount,
-            Sender(spender),
-        );
-
-        assert_eq!(
-            fixture.balance_of(Key::from(owner)),
-            Some(owner_balance_before - transfer_amount),
-            "should decrease balance of the owner"
-        );
-        assert_eq!(
-            fixture.allowance(Key::from(owner), Key::from(spender)),
-            Some(approve_amount - transfer_amount),
-            "should decrease allowance of the spender"
-        );
-        assert_eq!(
-            fixture.balance_of(Key::from(recipient)),
-            Some(transfer_amount),
-            "recipient should receive tokens"
         );
     }
 
